058691b2df7126e7ad277d8129d4d4884360280e
周一斐 <1545019776@qq.com>
2026-04-14 13:11:46 +0800

    [feat] 实现敏感词审查类

--- sensitive_filter/src/main/java/com/memoecho/sensitive_filter/service/SensitiveWordsService.java
+++ sensitive_filter/src/main/java/com/memoecho/sensitive_filter/service/SensitiveWordsService.java
@@ -1,6 +1,7 @@
 package com.memoecho.sensitive_filter.service;
 
 import com.hankcs.algorithm.AhoCorasickDoubleArrayTrie;
+import com.memoecho.sensitive_filter.pojo.SensitiveWords;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
@@ -15,12 +16,14 @@ import java.util.TreeMap;
 @Slf4j(topic = "敏感词筛查")
 public class SensitiveWordsService {
 
-    private AhoCorasickDoubleArrayTrie<String> acdat;
+    private AhoCorasickDoubleArrayTrie<SensitiveWords> acdat;
 
-    @Value("${}")
+    @Value("${bot.dict.cache_path:./sensitive_words.bin}")
     private String cachePath;
-    @Value("${}")
-    private String txtPath;
+    @Value("${bot.dict.level3_path:dict/sensitive_words_level3}")
+    private String txtLevel3Path;
+    @Value("${bot.dict.level2_path:dict/sensitive_words_level3}")
+    private String txtLevel2Path;
 
     @PostConstruct
     public void init(){
@@ -53,21 +56,41 @@ public class SensitiveWordsService {
 
     public void buildFromTxt(){
         long begin = System.currentTimeMillis();
-        TreeMap<String,String> tmp = new TreeMap<>();
+        TreeMap<String,SensitiveWords> tmp = new TreeMap<>();
+
+        loadFromTxt(txtLevel2Path,tmp);
+        loadFromTxt(txtLevel3Path,tmp);
+
+        acdat.build(tmp);
+        log.info("敏感词过滤器加载完毕，耗时:{} ms。",System.currentTimeMillis() - begin);
+    }
+
+    public void loadFromTxt(String path,TreeMap<String,SensitiveWords> tmp){
         try(InputStream is =
-                    new ClassPathResource(txtPath).getInputStream();
+                    new ClassPathResource(txtLevel2Path).getInputStream();
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))){
-
-
+            String line;
+            while((line = reader.readLine())!=null){
+                line = line.trim();
+                if(line.isEmpty() || line.startsWith("#")){
+                    continue;
+                }
+                tmp.put(line,new SensitiveWords(SensitiveWords.Level.level2,1));
+            }
 
         } catch (Exception e){
-
+            log.error("{}路径文件敏感词读取失败，请查看原因：",path,e);
         }
-
     }
 
     public void saveToCache(File cacheFile){
-
+        try(ObjectOutputStream oos =
+                    new ObjectOutputStream(new FileOutputStream(cacheFile));) {
+            acdat.save(oos);
+            log.info("保存缓存成功。");
+        } catch (Exception e){
+            log.error("保存缓存失败,错误信息：",e);
+        }
     }
 }
